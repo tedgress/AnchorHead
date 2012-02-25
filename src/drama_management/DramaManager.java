25b7d42f447808f1be7173fe0ccb2e9d468b235e
Josh Datko <jbdatko@gmail.com>
2012-02-24 19:59:38 -0500

    Added basic flow

--- src/drama_management/DramaManager.java
+++ src/drama_management/DramaManager.java
@@ -1,97 +1,48 @@
 package drama_management;
 
-import ifgameengine.IFAction;
+import ifgameengine.AbstractHint;
 import ifgameengine.IFGameState;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 
-import storyengine.IFPlotPoint;
 import storyengine.IFStoryState;
 
 public class DramaManager implements IFDramaManager {
-
-	IFStoryState currentStoryState = null;
-	IFGameState currentGameState = null;
-	HashSet<IFPlotPoint> totalPlot = null;
 	
+	protected PlayerModel model = new PlayerModel();
+	protected HintRepository hint_repo = new HintRepository();
+	protected PlayerPatron patron = new PlayerPatron();
 	
-	private static final int ONE_MINUTE_IN_CYCLES = 1200;
-	
-	List<String> failedInput = new LinkedList<String>();
-	String failedInputResponse = "Say what?";
-
-	protected IFGameState getCurrentGameState() {
-		return currentGameState;
-	}
-
-	protected void setCurrentGameState(IFGameState currentGameState) {
-		this.currentGameState = currentGameState;
-	}
-
-	protected IFStoryState getCurrentStoryState() {
-		return currentStoryState;
-	}
-
-	protected void setCurrentStoryState(IFStoryState currentState) {
-		this.currentStoryState = currentState;
-	}
 
 	@Override
 	public void update(IFStoryState story, IFGameState game,
 			List<String> logOutput) {
 
-		// Set current game information
-		this.setCurrentStoryState(story);
-		this.setCurrentGameState(game);
-		
-		if (game.getCycle() % ONE_MINUTE_IN_CYCLES == 0) {
-			this.talkToSelf("I'm not feeling so good.");
+		//Is the player frustrated?
+		if ( this.model.isPlayerFrustrated(story, game, logOutput)){
+			//Let's help him out, grab a hint
+			AbstractHint hint = this.hint_repo.getHint(story, game, logOutput);
 			
+			if (null != hint){
+				//Attempt to offer the hint to the player
+				boolean hintAccepted = this.patron.hintPlayer(hint, story, game, logOutput);
+				
+				//Update the hint status
+				this.hint_repo.updateHint(hint, hintAccepted);
+				
+			}
 		}
 		
-		//System.out.println(this.getRemainingPlotPoints().toString());
-		
-		
+	
 
 	}
 	
-	protected Collection<IFPlotPoint> getRemainingPlotPoints(){
-		if (this.totalPlot == null)
-			this.totalPlot = new HashSet<IFPlotPoint>(this.getCurrentStoryState().getStory().getPlotPoints());
-		
-		HashSet<IFPlotPoint> remaining = new HashSet<IFPlotPoint>(this.totalPlot);
-			
-		remaining.removeAll( this.getCurrentStoryState().getPlotPointsVisited() );
-		
-		return remaining;
-		
-		
-	}
-
-	protected void talkToSelf(String message){
-		
-		if (null == this.getCurrentStoryState()
-				|| null == this.getCurrentGameState())
-			throw new NullPointerException("Null States");
-
-		this.getCurrentGameState().enqueueAction(
-				new IFAction("player", "talk", "player", "reply", message),
-				this.getCurrentStoryState());
-
-	}
 
 	@Override
 	public String informBadInput(String unrecognized) {
 		
-		//Add the input to the queue
-		this.failedInput.add(unrecognized);
-		
 		//Decide what to do an return a response.
 		//NOTE: This will occur in the GUI thread...
-		return this.failedInputResponse;
+		return this.model.addBadUserInput(unrecognized);
 		
 	
 	}
